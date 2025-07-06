fb298f2411dc06e3bb4fdf4bc1bfefea4eb7a9f6
damnthonyy <mahassadijoseph@gmail.com>
2025-07-07 00:59:28 +0200

    feat(IndexPage): add landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
-    </div>
+    <main class="flex flex-col items-center justify-center gap-[30px]">
+        <div class="flex flex-col items-center justify-center gap-[10px]">
+            <span class="text-[48px] font-medium">
+                ✍️ Minimaliste & clair
+            </span>
+            <span class="text-[20px] font-medium">
+                Écrivez. Classez. Retrouvez.
+            </span>
+            <span class="text-[16px] font-500 font-weight-[500]">
+                Un espace de prise de notes simple, rapide et rien que pour vous.
+            </span>
+        </div>
+        <div class="flex items-center gap-[20px] ">
+            <Button variant="primary" size="sm">
+                <span>
+                    Sign in
+                </span>
+            </Button>
+            <Button variant="outline" size="sm">
+                <span>
+                    Sign up
+                </span>
+            </Button>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
+import Button from '~/components/ui/Button.vue';
 
+definePageMeta({
+    layout: 'custom',
+})
 </script>
 
 <style scoped>
