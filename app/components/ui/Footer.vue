d5fa19f91632fd10cbf5b7a797646e11d09e1c16
damnthonyy <mahassadijoseph@gmail.com>
2025-07-07 00:58:31 +0200

    feat(Footer): add footer

---
+++ app/components/ui/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <span class="text-[14px] text-center font-medium">
+            made by <a href="https://github.com/damnthonyy" target="_blank" class="text-blue-500">antoine Mahassadi</a>
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
