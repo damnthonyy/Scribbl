41314c3fe8a745c2a0f340944d898265286cda7a
damnthonyy <mahassadijoseph@gmail.com>
2025-07-07 00:58:15 +0200

    feat(custom-layout): add custom layout component

---
+++ app/layouts/custom.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <Header/>
+        <main class="flex-1">
+            <slot/>
+        </main>
+        <div class="flex justify-center p-[20px]">
+            <Footer/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Header from '~/components/ui/Header.vue';
+import Footer from '~/components/ui/Footer.vue';
+</script>
