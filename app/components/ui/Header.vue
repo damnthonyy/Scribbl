01dde0fd793c7b376d691c3f4381a98698e548dd
damnthonyy <mahassadijoseph@gmail.com>
2025-07-07 00:58:43 +0200

    feat(Header): add header

---
+++ app/components/ui/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex justify-between items-center w-full p-[20px] ">
+        <span class="font-medium text-[24px]">
+            Scribbl
+        </span>
+    </div>
+</template>
