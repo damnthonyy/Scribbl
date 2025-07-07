475d570e829c51240d36f6e26c343743fae1bead
damnthonyy <mahassadijoseph@gmail.com>
2025-07-07 12:38:46 +0200

    fix(Index.vue): adjust responsive

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,17 @@
 <template>
     <main class="flex flex-col items-center justify-center gap-[30px] px-[20px] py-[40px]">
-        <div class="flex flex-col items-center justify-center gap-[10px] text-center">
-            <span class="text-[32px] md:text-[48px] font-medium sm:text-[21px]">
+        <div class="flex flex-col items-center justify-center gap-[10px] text-center mt-[20vh]">
+            <span class="text-[21px] sm:text-[21px] md:text-[48px] font-medium">
                 ✍️ Minimaliste & clair
             </span>
-            <span class="text-[20px] font-medium">
+            <span class="text-[20px] font-weight-[300] sm:text-[16px]">
                 Écrivez. Classez. Retrouvez.
             </span>
-            <span class="text-[16px] font-500 font-weight-[500]">
+            <span class="text-[14px] font-light">
                 Un espace de prise de notes simple, rapide et rien que pour vous.
             </span>
         </div>
-        <div class="flex flex-col sm:flex-row items-center gap-[15px] sm:gap-[20px]">
+        <div class="flex justify-center align-center sm:flex items-center justify-center gap-[15px] md:gap-[20px]">
             <Button variant="primary" size="sm">
                 <span>
                     Sign in
