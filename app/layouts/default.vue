91844a3d8f13bf75570e30cd10aaa0da62e4557b
damnthonyy <mahassadijoseph@gmail.com>
2025-07-23 12:21:51 +0200

    feat(layout): add layout for dashboard

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="flex p-y-[18px] p-x-[20px] align-center">
+        <div class="w-full flex-none bg-red-200">
+            <slot/>
+        </div>
+        <div class="w-full flex-1 bg-blue-200">
+            <slot/>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
